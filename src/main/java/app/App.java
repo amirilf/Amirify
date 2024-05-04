14a6fa5081eef0dcce7a6e6565f1d216309949f6
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-05-04 17:10:00 +0330

    Add starter page

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,15 +1,23 @@
 package app;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        stage.setScene(new Scene(new Pane()));
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/AuthPage.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Amirify");
         stage.show();
     }
 
