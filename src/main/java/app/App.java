1a382b2a4c4d6ae15f09bd5245ca0f455fc631e0
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-05-07 20:39:46 +0330

    Update top and left bars & add some style for them

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -6,15 +6,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/AuthPage.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/base/Base.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("images/logo.png")));
         stage.setScene(scene);
         stage.setResizable(false);
         stage.setTitle("Amirify");
