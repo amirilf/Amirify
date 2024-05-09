0cf07c5c8e595c48059789f9e94afe214643d146
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-05-09 12:10:07 +0330

    Fix slide player bugs & add repeat and shuffle to bottom bar

--- src/main/java/app/gui/BottomBarController.java
+++ src/main/java/app/gui/BottomBarController.java
@@ -17,6 +17,10 @@ public class BottomBarController {
 
     String playIconPath = "/app/images/icon-play.png";
     String pauseIconPath = "/app/images/icon-pause.png";
+    String repeatOnIconPath = "/app/images/icon-repeat-on.png";
+    String repeatOffIconPath = "/app/images/icon-repeat-off.png";
+    String shuffleOnIconPath = "/app/images/icon-shuffle-on.png";
+    String shuffleOffIconPath = "/app/images/icon-shuffle-off.png";
 
     @FXML
     private Label startTimeLabel;
@@ -33,7 +37,16 @@ public class BottomBarController {
     @FXML
     private ImageView playPauseButton;
 
+    @FXML
+    private ImageView repeatIcon;
+
+    @FXML
+    private ImageView shuffleIcon;
+
+    private Timeline timeline = new Timeline();
     private boolean isPlaying = false;
+    private boolean repeat = false;
+    private boolean shuffle = false;
 
     private DoubleProperty currentDurationProperty = new SimpleDoubleProperty(0);
     private DoubleProperty totalDurationProperty = new SimpleDoubleProperty(200);
@@ -64,6 +77,10 @@ public class BottomBarController {
             setImage(playPauseButton, playIconPath);
         } else {
             setImage(playPauseButton, pauseIconPath);
+            if (currentDurationProperty.get() >= totalDurationProperty.get()) {
+                currentDurationProperty.set(0);
+            }
+            timeline.play();
         }
         isPlaying = !isPlaying;
     }
@@ -74,15 +91,45 @@ public class BottomBarController {
     }
 
     private void simulateMusicPlayback() {
-        Timeline timeline = new Timeline(
+        timeline.getKeyFrames().add(
                 new KeyFrame(
                         Duration.seconds(1),
                         event -> {
                             if (isPlaying) {
                                 currentDurationProperty.set(currentDurationProperty.get() + 1);
+                                if (currentDurationProperty.get() >= totalDurationProperty.get()) {
+                                    if (repeat) {
+                                        currentDurationProperty.set(0);
+                                    } else {
+                                        // no repeating
+                                        isPlaying = false;
+                                        setImage(playPauseButton, playIconPath);
+                                        timeline.stop();
+                                    }
+                                }
                             }
                         }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
+    }
+
+    @FXML
+    private void handleRepeatClick() {
+        repeat = !repeat;
+        if (repeat) {
+            repeatIcon.setImage(new Image(getClass().getResourceAsStream(repeatOnIconPath)));
+        } else {
+            repeatIcon.setImage(new Image(getClass().getResourceAsStream(repeatOffIconPath)));
+        }
+    }
+
+    @FXML
+    private void handleShuffleClick() {
+        shuffle = !shuffle;
+        if (shuffle) {
+            shuffleIcon.setImage(new Image(getClass().getResourceAsStream(shuffleOnIconPath)));
+        } else {
+            shuffleIcon.setImage(new Image(getClass().getResourceAsStream(shuffleOffIconPath)));
+        }
     }
 }
