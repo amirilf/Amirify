d643f31fdd6168f81866d70ce72bd6840adfcb64
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-05-13 21:39:29 +0330

    Complete search basic structure

--- src/main/java/app/gui/SearchController.java
+++ src/main/java/app/gui/SearchController.java
@@ -1,5 +1,24 @@
 package app.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class SearchController {
 
+    @FXML
+    private TextField searchInput;
+
+    @FXML
+    private void handleSearchButtonClick() {
+        String searchText = searchInput.getText();
+
+        if (searchText.equals("")) {
+            // it's empty
+            searchInput.setStyle("-fx-border-color: red;");
+            System.out.println("empty");
+        } else {
+            searchInput.setStyle("-fx-border-color: white;");
+            System.out.println("search: " + searchText);
+        }
+    }
 }
