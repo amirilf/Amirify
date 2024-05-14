e7d809b930d63b9abb9cf344e38ebdfd50bdb3b9
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-05-14 11:04:58 +0330

    Add result page base template

---
+++ src/main/java/app/gui/ResultController.java
@@ -0,0 +1,48 @@
+package app.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+public class ResultController {
+
+    @FXML
+    private void showPlayMedia(MouseEvent event) {
+        if (event.getSource() instanceof VBox) {
+            VBox vbox = (VBox) event.getSource();
+            ImageView playMediaImageView = findPlayMediaImageView(vbox);
+            if (playMediaImageView != null) {
+                playMediaImageView.setVisible(true);
+            }
+
+        }
+    }
+
+    @FXML
+    private void hidePlayMedia(MouseEvent event) {
+        if (event.getSource() instanceof VBox) {
+            VBox vbox = (VBox) event.getSource();
+            ImageView playMediaImageView = findPlayMediaImageView(vbox);
+            if (playMediaImageView != null) {
+                playMediaImageView.setVisible(false);
+            }
+        }
+    }
+
+    private ImageView findPlayMediaImageView(VBox vbox) {
+        for (Node node : vbox.getChildren()) {
+            if (node instanceof StackPane) {
+                StackPane stackPane = (StackPane) node;
+                for (Node child : stackPane.getChildren()) {
+                    if (child instanceof ImageView && child.getStyleClass().contains("playMediaImage")) {
+                        return (ImageView) child;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
