cefd190a10138c8c9833153853bbcd1d275c6df1
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-05-17 19:06:07 +0330

    Update genres in model

--- src/main/java/app/model/Genre.java
+++ src/main/java/app/model/Genre.java
@@ -1,5 +1,37 @@
 package app.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public enum Genre {
-    Rock, Pop, Jazz, HipHop, Country, TrueCrime, Society, Interview, History;
+    Pop,
+    Rock,
+    HipHop,
+    RnB,
+    Soul,
+    Electronic,
+    Dance,
+    Country,
+    Jazz,
+    Classical,
+    Reggae,
+    Metal,
+    Indie,
+    Alternative,
+    Blues,
+    Latin,
+    Folk,
+    Punk,
+    Funk,
+    Gospel,
+    Techno,
+    Trance,
+    House;
+
+    public static ArrayList<String> getGenreNames() {
+        return new ArrayList<>(Arrays.asList(
+                Arrays.stream(Genre.values())
+                        .map(Genre::name)
+                        .toArray(String[]::new)));
+    }
 }
