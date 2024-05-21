916e7ef5e26b377254c365bcc02cb9db7eec2fd5
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-05-21 11:16:40 +0330

    Complete playlist basic structure

--- src/main/java/app/gui/page/PlaylistController.java
+++ src/main/java/app/gui/page/PlaylistController.java
@@ -1,5 +1,37 @@
 package app.gui.page;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class PlaylistController {
+
+    @FXML
+    private Label type;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label desc;
+
+    @FXML
+    private Label owner;
+
+    // in albums it's date and in playlists it's like
+    @FXML
+    private Label datelike;
+
+    @FXML
+    private Label numberOfAudios;
+
+    @FXML
+    private Label totalTime;
+
+    @FXML
+    private ImageView cover;
+
+    @FXML
+    private ImageView playButton;
 
 }
